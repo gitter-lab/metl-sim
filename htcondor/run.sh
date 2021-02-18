9b2d5f6a12eb5b3bc63fcc931bfa97a824ccfd87
Sam Gelman <samgelman1@gmail.com>
2021-02-18 16:49:00 -0600

    Updating HTCondor rosetta files (incomplete)

--- htcondor/run.sh
+++ htcondor/run.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 # echo some HTCondor job information
-echo _CONDOR_JOB_IWD $_CONDOR_JOB_IWD
-echo Cluster $cluster
-echo Process $process
-echo RunningOn $runningon
+echo "Date: $(date)"
+echo "Host: $(hostname)"
+echo "System: $(uname -spo)"
+echo "_CONDOR_JOB_IWD: $_CONDOR_JOB_IWD"
+echo "Cluster: $CLUSTER"
+echo "Process: $PROCESS"
+echo "RunningOn: $RUNNINGON"
 
 # this makes it easier to set up the environments, since the PWD we are running in is not $HOME
 export HOME=$PWD
 
-# move transferred rosetta files to the rosetta working directory
-mv 3_10_* rosetta_working_dir
-cd rosetta_working_dir
-chmod +x 3_10_rosetta_scripts.static.linuxgccrelease
-chmod +x 3_10_residue_energy_breakdown.static.linuxgccrelease
-# this combines the databases into a single database? why is this necessary, why not just
-# make database.tar and transfer it
+# download rosetta binaries and database from SQUID to the rosetta working directory
+cd rosetta_working_dir || exit
+wget http://proxy.chtc.wisc.edu/SQUID/sgelman2/rosetta/3_10_databaseaa
+wget http://proxy.chtc.wisc.edu/SQUID/sgelman2/rosetta/3_10_databaseab
+wget http://proxy.chtc.wisc.edu/SQUID/sgelman2/rosetta/3_10_databaseac
+wget http://proxy.chtc.wisc.edu/SQUID/sgelman2/rosetta/3_10_rosetta_scripts.static.linuxgccrelease
+
+# set the rosetta binaries to be executable
+chmod +x rosetta_scripts.static.linuxgccrelease
+
+# this combines the split database tar files into a single database
 cat 3_10_databasea* > database.tar
-tar -xf database.tar
+tar -xf database.
+
+# cd back to the root directory
 cd ..
 
 # set up miniconda and add it to path
@@ -25,7 +34,7 @@ wget -q https://repo.anaconda.com/miniconda/Miniconda3-py38_4.9.2-Linux-x86_64.s
 bash Miniconda3-py38_4.9.2-Linux-x86_64.sh -b -p ~/miniconda3
 export PATH=$HOME/miniconda3/bin:$PATH
 
-# set up appropriate conda environment
+# set up conda environment from env.yml
 source "$HOME/miniconda3/etc/profile.d/conda.sh"
 hash -r
 conda config --set always_yes yes --set changeps1 no
@@ -44,6 +53,6 @@ chmod +x ~/code/relax.sh
 chmod +x ~/code/clean_up_working_dir.sh
 
 # launch our python run script with argument file number
-python code/run.py args_gb1/$1.txt --job_id=$1 --pdb_fn="pdb_files/gb1_clean_0007.pdb"
+python code/run.py args_gb1/"$1".txt --job_id="$1" --pdb_fn="pdb_files/gb1_clean_0007.pdb"
 # python code/run.py args_gb1/random-10.txt --job_id=$1 --pdb_fn="pdb_files/gb1_clean_0007.pdb"
 
