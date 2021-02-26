1440e1fce26470b20758306e45a0e9a9b7b6c9d9
Sam Gelman <samgelman1@gmail.com>
2021-02-26 16:59:24 -0600

    Add most (all?) rosetta binaries and files needed for rosettafy

--- code/rosetta_distr.py
+++ code/rosetta_distr.py
@@ -5,9 +5,15 @@ import argparse
 
 def main(args):
 
+    # rosettafy was designed to work with Rosetta 2020.50.61505
+
     to_copy = ["source/bin/relax.static.linuxgccrelease",
-               "source/bin/rosetta_scripts.static.linuxgccrelease"]
-    pass
+               "source/bin/rosetta_scripts.static.linuxgccrelease",
+               "database",
+               "source/src/apps/public/relax_w_allatom_cst/clean_pdb_keep_ligand.py",
+               "source/src/apps/public/relax_w_allatom_cst/amino_acids.py"]
+
+    # copy the scripts to the output directory but maintain the full paths
 
 
 if __name__ == "__main__":
@@ -20,5 +26,10 @@ if __name__ == "__main__":
                              "other files that are needed for this script",
                         type=str,
                         default="/home/sg/Desktop/rosetta/rosetta_bin_linux_2020.50.61505_bundle/main")
+
+    parser.add_argument("--out_dir",
+                        help="The output directory where to place the minimum rosetta binaries and scripts",
+                        type=str,
+                        default="rosetta_distribution")
 
     main(parser.parse_args())
