1167b4910f46f57b828dc6252a81002703c29b49
Sam Gelman <samgelman1@gmail.com>
2021-04-15 11:25:46 -0500

    Add additional centroid & filter terms to database spec

--- code/database.py
+++ code/database.py
@@ -1,12 +1,13 @@
 """creating, storing, and accessing variant database
 https://www.sqlitetutorial.net/sqlite-python/creating-database/"""
 
+from os.path import isfile
 import sqlite3
 import sys
+import argparse
 
 
 def create_tables(con):
-
     # retrieve table creation commands from file
     ct_fn = "variant_database/create_tables.sql"
     with open(ct_fn, "r") as f:
@@ -21,16 +22,41 @@ def create_tables(con):
         cur.execute(command)
 
 
-def main():
+def main(args):
 
-    db_fn = "variant_database/database.db"
-    con = sqlite3.connect(db_fn)
+    if args.mode == "create":
+        # create a new database with the proper tables for storing energies
+        if isfile(args.db_fn):
+            print("error: database already exists: {}".format(args.db_fn))
+        else:
+            con = sqlite3.connect(args.db_fn)
+            create_tables(con)
+            con.commit()
+            con.close()
 
-    create_tables(con)
-
-    con.commit()
-    con.close()
+    elif args.mode == "add":
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        fromfile_prefix_chars="@")
+
+    parser.add_argument("mode",
+                        help="run mode",
+                        type=str,
+                        choices=["create", "add"])
+
+    parser.add_argument("--db_fn",
+                        help="path to database file",
+                        type=str,
+                        default="variant_database/database.db")
+
+    parser.add_argument("--condor_main_dir",
+                        type=str,
+                        help="path to the parent condor directory for run to process")
+
+    main(parser.parse_args())
+
