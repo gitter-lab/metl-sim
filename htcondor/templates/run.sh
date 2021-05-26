d1572adaf39b0145aeef31cbdd58fb29c74cab51
Sam Gelman <samgelman1@gmail.com>
2021-05-26 18:30:03 -0500

    Add debug echo statements to HTCondor run file

--- htcondor/templates/run.sh
+++ htcondor/templates/run.sh
@@ -21,6 +21,7 @@ export HOME=$PWD
 
 # untar the code repo (transferred from submit node), removing the enclosing folder with strip-components
 # switched to a static filename so there's less need to have github tag everywhere
+echo "Extracting code.tar.gz"
 tar -xf code.tar.gz --strip-components=1
 
 # download the rosetta distribution from SQUID
@@ -31,15 +32,20 @@ tar -xf code.tar.gz --strip-components=1
 #wget -q http://proxy.chtc.wisc.edu/SQUID/sgelman2/$SQUID_DIR/rosetta_min_enc.tar.gz.ac
 
 # combine the split database tar files into a single database
+echo "Combining Rosetta split files"
 cat rosetta_min_enc.tar.gz.* > rosetta_min_enc.tar.gz
 rm rosetta_min_enc.tar.gz.*
+
 # decrypt
+echo "Decrypting Rosetta"
 openssl version # echo the version for my knowledge
 # this was encrypted w/ openssl v > 1.1.0, which uses default digest sha256
 # include "-md sha256" for decrypt compatibility with older versions that used md5
 openssl enc -d -aes256 -md sha256 -in rosetta_min_enc.tar.gz -out rosetta_min.tar.gz -pass pass:R0S3774123
 rm rosetta_min_enc.tar.gz
+
 # extract
+echo "Extracting Rosetta"
 tar -xf rosetta_min.tar.gz
 rm rosetta_min.tar.gz
 
@@ -50,11 +56,13 @@ rm rosetta_min.tar.gz
 # simply a convenient way to keep track of versioning for this package which was created by hand
 #wget -q http://proxy.chtc.wisc.edu/SQUID/sgelman2/rosettafy_env_v0.4.tar.gz
 # these lines handle setting up the environment
+echo "Setting up Python environment"
 export PATH
 mkdir rosettafy_env
 tar -xzf rosettafy_env_v0.4.tar.gz -C rosettafy_env
 . rosettafy_env/bin/activate
 
 # launch our python run script with argument file number
+echo "Launching energize.py"
 python3 code/energize.py @energize_args.txt --variants_fn="${PROCESS}.txt" --cluster="$CLUSTER" --process="$PROCESS" --commit_id="$GITHUB_TAG"
 
